68e4912eaf35c8572d412cc718673b222aeef146
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-07-24 16:22:29 +0200

    refactor(MainHeader): remove unused imports and variables

--- src/views/main/MainHeader/MainHeader.vue
+++ src/views/main/MainHeader/MainHeader.vue
@@ -4,7 +4,6 @@ import BaseIconButton from '@/components/Buttons/BaseIconButton.vue'
 import AddFolderIcon from '~icons/icons-16/add-folder'
 import MoreIcon from '~icons/icons-16/more'
 import WorkspaceDropdown from '@/components/Dropdown/WorkspaceDropdown.vue'
-import { useSettingsStore } from '@/stores/settings'
 import { useI18n } from 'vue-i18n'
 import { useCategoryStore } from '@/stores/category'
 import { useCategoryTabs } from './useTabs'
@@ -13,17 +12,12 @@ import { useWorkspaceMenu } from './useWorkspaceMenu'
 
 const { t } = useI18n()
 
-const settingsStore = useSettingsStore()
 const categoryStore = useCategoryStore()
 
 const { tabs, currentTab } = useCategoryTabs(t)
 const { openMoreMenu } = useMoreMenu(t)
 const { currentWorkspaceName, openWorkspaceMenu } =
   useWorkspaceMenu(t)
-
-async function handleThemeClick() {
-  await settingsStore.nextLanguage()
-}
 
 async function onAddCategoryClick() {
   if (!categoryStore.categories) return
