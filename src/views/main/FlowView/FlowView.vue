4a7842062d33627262eeba2a2af966eb7f8e7a75
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-08-16 16:54:11 +0200

    feat(FlowView): add placeholder

--- src/views/main/FlowView/FlowView.vue
+++ src/views/main/FlowView/FlowView.vue
@@ -1,10 +1,28 @@
-<script setup lang="ts">
-import MainHeader from '../MainHeader/MainHeader.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <MainHeader />
-  FlowView
+  <div class="flow-view">
+    <span class="hint">FlowView</span>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '@/assets/styles/text-styles' as *;
+
+.flow-view {
+  position: relative;
+  display: flex;
+  flex: 1;
+  margin-bottom: var(--bottom-container-height);
+}
+
+.hint {
+  @include text-caption;
+  margin: auto;
+  color: var(--text-muted);
+  background-color: var(--surface);
+  border-radius: var(--corner-full);
+  padding: var(--space-4) var(--space-12);
+  outline: var(--stroke-subtle) solid var(--border-muted);
+}
+</style>
