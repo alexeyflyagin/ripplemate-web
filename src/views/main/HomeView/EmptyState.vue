031e42e42694f7ef12b3a529ab8b33f6d768c9fd
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-08-17 21:30:44 +0200

    feat: add EmptyState component for HomeView

---
+++ src/views/main/HomeView/EmptyState.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import type { Component } from 'vue'
+
+defineProps<{
+  icon: Component
+  title: string
+  subtitle: string
+}>()
+</script>
+
+<template>
+  <div class="empty-state">
+    <div class="empty-state__content">
+      <component class="empty-state__icon" :is="icon" />
+      <h3 class="empty-state__title">
+        {{ title }}
+      </h3>
+      <h6 class="empty-state__subtitle">
+        {{ subtitle }}
+      </h6>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/text-styles' as *;
+
+.empty-state {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &__content {
+    display: flex;
+    width: 280px;
+    flex-direction: column;
+    align-items: center;
+    box-sizing: border-box;
+    padding: var(--space-24);
+    color: var(--text-placeholder);
+    text-align: center;
+  }
+
+  &__icon {
+    width: 60px;
+    height: 60px;
+  }
+
+  &__title {
+    @include text-title;
+    margin-top: var(--space-12);
+  }
+
+  &__subtitle {
+    @include text-caption;
+    margin-top: var(--space-8);
+  }
+}
+</style>
