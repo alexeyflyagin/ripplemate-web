887d45276f837c285ea8c7b3b20c8efc7a502597
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-08-19 14:28:50 +0200

    feat: add AnswerButton component

---
+++ src/components/Buttons/AnswerButton.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    label: string
+    color?: 'green' | 'yellow' | 'red'
+  }>(),
+  {
+    color: 'green',
+  },
+)
+
+const emit = defineEmits<{
+  click: []
+}>()
+</script>
+
+<template>
+  <button
+    class="answer-button"
+    :class="{
+      [`answer-button--${color}`]: color !== 'green',
+    }"
+    @click.stop="emit('click')"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/mixins' as *;
+@use '@/assets/styles/text-styles' as *;
+
+.answer-button {
+  @include text-label-emphasized;
+  position: relative;
+  padding: var(--space-16);
+  border: none;
+  border-radius: var(--corner-large);
+  background-color: var(--easy);
+  color: var(--white);
+  transition: transform 0.16s var(--ease-standard);
+  user-select: none;
+  cursor: pointer;
+
+  &::after {
+    content: '';
+    position: absolute;
+    inset: 0;
+    border-radius: inherit;
+    background-color: var(--white);
+    transition: opacity 0.2s var(--ease-standard);
+    opacity: 0;
+  }
+
+  &--yellow {
+    background-color: var(--good);
+  }
+
+  &--red {
+    background-color: var(--hard);
+  }
+
+  @media (hover: hover) {
+    &:hover::after {
+      opacity: var(--opacity-8);
+    }
+  }
+
+  &:focus-visible {
+    @include focus-outline;
+  }
+
+  &:active {
+    transform: scale(0.94);
+  }
+}
+</style>
