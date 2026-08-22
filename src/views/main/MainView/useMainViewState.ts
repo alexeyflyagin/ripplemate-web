ab0092c0352d97bfd434c3d00f85a7ce1bb2fb20
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-08-22 10:02:02 +0200

    feat: add history navigation to exit editing mode

--- src/views/main/MainView/useMainViewState.ts
+++ src/views/main/MainView/useMainViewState.ts
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 
 export const VIEWS = ['home', 'flow'] as const
 export type MainViewTypes = (typeof VIEWS)[number]
@@ -12,6 +12,37 @@ export type NavBarMode =
 export function useMainViewStates() {
   const currentView = ref<MainViewTypes>('home')
   const mode = ref<NavBarMode>('default')
+  let defaultHistory = true
+
+  watch(mode, (v) => {
+    if (v !== 'default') {
+      history.pushState({ mode: v }, '')
+      defaultHistory = false
+      window.addEventListener('popstate', onPopState)
+      window.addEventListener('keydown', onKeyDown)
+    } else if (!defaultHistory) {
+      history.back()
+      clearPopState()
+    }
+  })
+
+  function onPopState() {
+    mode.value = 'default'
+    clearPopState()
+  }
+
+  function onKeyDown(e: KeyboardEvent) {
+    if (e.key === 'Escape') {
+      mode.value = 'default'
+      clearPopState()
+    }
+  }
+
+  function clearPopState() {
+    defaultHistory = true
+    window.removeEventListener('popstate', onPopState)
+    window.removeEventListener('keydown', onKeyDown)
+  }
 
   return { currentView, mode }
 }
