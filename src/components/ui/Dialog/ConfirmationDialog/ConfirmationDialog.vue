6413d04f719b6024a9bd888eba098446fd04e159
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-08-23 21:22:43 +0200

    feat: add ConfirmationDialog

---
+++ src/components/ui/Dialog/ConfirmationDialog/ConfirmationDialog.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { BaseButton } from '@/components/ui/Button/BaseButton'
+
+defineProps<{
+  title: string
+  caption: string
+  confirm: string
+  cancel: string
+}>()
+
+const emit = defineEmits<{
+  confirm: []
+  cancel: []
+}>()
+</script>
+
+<template>
+  <div class="dialog-overlay">
+    <div class="dialog">
+      <div class="dialog__content">
+        <h1 class="dialog__title">{{ title }}</h1>
+        <p class="dialog__caption" v-html="caption" />
+      </div>
+      <div class="dialog__actions">
+        <BaseButton
+          class="dialog__confirm"
+          :label="confirm"
+          variant="danger-text"
+          @click="emit('confirm')"
+        />
+        <BaseButton
+          class="dialog__cancel"
+          :label="cancel"
+          @click="emit('cancel')"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/blur' as *;
+@use '@/assets/styles/text-styles' as *;
+@use '@/assets/styles/shadows' as *;
+
+.dialog-overlay {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  inset: 0;
+  background-color: color-mix(
+    in srgb,
+    black 40%,
+    transparent
+  );
+}
+
+.dialog {
+  @include elevation-4;
+  @include background-blur-10;
+  display: flex;
+  width: 100%;
+  max-width: var(--max-content-width-300);
+  flex-direction: column;
+  background-color: var(--surface-60);
+  border-radius: var(--corner-xxxlarge);
+  border: var(--stroke-subtle) solid var(--border);
+  margin: var(--space-40);
+}
+
+.dialog__content {
+  display: flex;
+  flex-direction: column;
+  gap: var(--space-24);
+  padding: var(--space-24);
+}
+
+.dialog__title {
+  @include text-title;
+  color: var(--text);
+}
+
+.dialog__caption {
+  @include text-caption;
+  color: var(--text-muted);
+}
+
+.dialog__actions {
+  display: flex;
+  justify-content: flex-end;
+  padding: 0 var(--space-12) var(--space-12);
+}
+</style>
