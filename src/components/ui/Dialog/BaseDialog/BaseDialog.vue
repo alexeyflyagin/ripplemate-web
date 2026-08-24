50c9bd237e1875da9c00e0adf3f59a7b3e9be164
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-08-24 11:19:54 +0200

    feat: add BaseDialog, BaseDialogHeader ui components

---
+++ src/components/ui/Dialog/BaseDialog/BaseDialog.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { useFocusTrap } from '@vueuse/integrations/useFocusTrap'
+import { useTemplateRef } from 'vue'
+
+withDefaults(
+  defineProps<{
+    maxWidth?: number
+  }>(),
+  {
+    maxWidth: 400,
+  },
+)
+
+const emit = defineEmits<{
+  overlay: []
+}>()
+
+const overlay = useTemplateRef('overlay')
+
+useFocusTrap(overlay, {
+  immediate: true,
+})
+</script>
+
+<template>
+  <div
+    ref="overlay"
+    class="overlay"
+    @click.stop="emit('overlay')"
+    @contextmenu.stop.prevent="emit('overlay')"
+  >
+    <div
+      class="base-dialog"
+      :style="{ 'max-width': `${maxWidth}px` }"
+      @click.stop
+      @contextmenu.stop.prevent
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/blur' as *;
+
+.overlay {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  inset: 0;
+  background-color: var(--scrim-70);
+  padding: var(--space-40);
+}
+
+.base-dialog {
+  @include background-blur-10;
+  position: relative;
+  display: flex;
+  width: 100%;
+  min-height: 100px;
+  min-width: 100px;
+  max-height: 100%;
+  overflow: auto;
+  background-color: var(--surface-80);
+  border-radius: var(--corner-xxxlarge);
+  border: var(--stroke-subtle) solid var(--border);
+}
+</style>
---
+++ src/components/ui/Dialog/BaseDialog/BaseDialogHeader.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import BaseIconButton from '@/components/ui/Button/BaseIconButton/BaseIconButton.vue'
+import CloseIcon from '~icons/icons-16/close'
+
+defineProps<{
+  title: string
+}>()
+
+const emit = defineEmits<{
+  close: []
+}>()
+</script>
+
+<template>
+  <div class="base-dialog-header">
+    <h1 class="base-dialog-header__title">{{ title }}</h1>
+    <BaseIconButton
+      class="base-dialog-header__close-button"
+      :icon="CloseIcon"
+      @click="emit('close')"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/text-styles' as *;
+
+.base-dialog-header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  padding: var(--space-12) var(--space-24);
+  color: var(--text);
+
+  &__title {
+    @include text-caption-emphasized;
+    flex: 1;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    margin-left: var(--space-8);
+  }
+
+  &__close-button {
+    margin-left: var(--space-16);
+  }
+}
+</style>
