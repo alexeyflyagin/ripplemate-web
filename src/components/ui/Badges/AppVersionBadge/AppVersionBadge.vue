399151d565bc95db3e64cb497505316e2dbdb9d7
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-09-14 03:06:18 +0200

    feat(Badges): add AppVersionBadge component

---
+++ src/components/ui/Badges/AppVersionBadge/AppVersionBadge.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+defineProps<{
+  version: string
+}>()
+</script>
+
+<template>
+  <div class="app-version-badge">{{ version }}</div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/styles/text-styles' as *;
+
+.app-version-badge {
+  @include text-label-emphasized;
+  color: var(--error);
+  background-color: var(--error-10);
+  border-radius: var(--corner-medium);
+  padding: var(--space-4) var(--space-8);
+}
+</style>
