301ff715bf2241f603703ab610752ac8a190324d
Alexey Flyagin <alexeyflyagin06@gmail.com>
2026-09-14 03:05:55 +0200

    fix(BaseIconButton): add initAnimation to BaseIconButtonData type

--- src/components/ui/Button/BaseIconButton/BaseIconButton.types.ts
+++ src/components/ui/Button/BaseIconButton/BaseIconButton.types.ts
@@ -10,5 +10,6 @@ export interface BaseIconButtonData {
   showSelectedBackground?: boolean
   disabled?: boolean
   hide?: boolean
+  initAnimation?: boolean
   onClick?: (event: MouseEvent | KeyboardEvent) => void
 }
